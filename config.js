87a5cd9373a895be71c2cd3094ac314f05061fae
Nectt <98696303+Nectt@users.noreply.github.com>
2022-02-23 02:19:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Thanks For Allah
-// AlyaaXzy
-// BOTCAHX
+// Nectt
+// NECTTBOT
 
 
 let fs = require('fs')
@@ -41,13 +41,13 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Exc'
-global.author = 'NyyXz'
+global.packname = 'Nectt'
+global.author = 'NECTT BOT'
 
 global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© R-Txzy' //change the watermark 
+global.watermark = '© Nectt' //change the watermark 
 global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
 
