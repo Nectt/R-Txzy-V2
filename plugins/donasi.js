81c2b9e1284dad0798bca02c9ff98edf8f178e52
Nectt <98696303+Nectt@users.noreply.github.com>
2022-02-23 03:27:01 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,9 +3,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 085730903853 [AlyaXd]
-├ Dana: 082221792667 [BOTCAHX]
-├ Pulsa: 081395861695 [BOTCAHX]
+├ GoPay: 088290013973 [NECTT]
+├ Dana: 088290013973 [NECTT]
+├ Pulsa: 088290013973 [NECTT]
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
